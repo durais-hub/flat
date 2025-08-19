a7783b7add984d386078aab40cd181a178349448
Durai <durairaj.g@gmail.com>
2025-08-19 18:03:11 +0530

    simple test run successfull

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { add } from '../src/index';
+
+describe('add function', () => {
+  it('should add two numbers correctly', () => {
+    expect(add(1, 2)).toBe(3);
+    expect(add(5, -3)).toBe(2);
+  });
+});

e0ae349562f0d98670a10402f10f57277ff82aab
Durai <durairaj.g@gmail.com>
2025-08-19 18:39:06 +0530

    define the skeleton and print object keys and value

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-export function add(a: number, b: number): number {
-  return a + b;
+
+// To have it meaningful, I preferred to use a meaningful object instead of the given sample.
+
+const input = {
+  user: {
+    name: "Durairaj",
+    age: 30,
+    favouriteBooks: ["Sheldon", "Vairamuthu"],
+    address: {
+      city: "Coimbatore",
+      zip: 641001
+    }
+  }
+};
+
+function flatGivenObjectToPath(obj: any): any  {
+  return Object.entries(obj).forEach((key, value) => console.log(`contents ${key},: ${value}`), {});
 }
 
-console.log(add(2, 3));
+
+const flattened = flatGivenObjectToPath(input);
+console.log(flattened);
